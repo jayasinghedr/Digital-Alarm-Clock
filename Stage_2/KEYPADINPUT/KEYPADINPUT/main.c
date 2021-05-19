074dafcee16fd936aec0984788c0056425a0d10e
Gayani Hewasura <gayanihewasura99@gmail.com>
2021-05-19 12:17:08 +0530

    Stage 2 files

--- Stage_2/KEYPADINPUT/KEYPADINPUT/main.c
+++ Stage_2/KEYPADINPUT/KEYPADINPUT/main.c
@@ -7,21 +7,21 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
-//#define F_CPU 8000000UL
+#define F_CPU 8000000UL
 uint8_t GetKey()
 {
 	uint8_t r,c;
-	PORTD |= 0x0F;
+	PORTD |= 0b00001111;
 	
 	for (c=0;c<3;c++)
 	{
-		DDRD &=~(0x7F);
-		DDRD |=(0x40>>c);
+		DDRD &=~(0b01111111);
+		DDRD |=(0b01000000>>c);
 		for (r=0;r<4;r++)
 		{
-			if(!(PIND & (0x08>>r)))
+			if(!(PIND & (0b00001000>>r)))
 			{
-				while(!(PIND & (0x08>>r)));
+				while(!(PIND & (0b00001000>>r)));
 				return(r*3+c +1);
 			}
 		}

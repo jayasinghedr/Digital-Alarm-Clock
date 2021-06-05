c84c89ee432d3394ccdd91ae15ab29f20a87d358
Dhanuja Jayasinghe <dhanujarumesh@gmail.com>
2021-06-05 12:10:24 +0530

    added code file

---
+++ full_product/src/alarm_clock/alarm_clock/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * alarm_clock.cpp
+ *
+ * Created: 04/06/2021 23:16:13
+ * Author : User
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
